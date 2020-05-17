531e8bbe06e5b1fe82b3f3910467eb9176ab2f83
shubham yadav <shubhamdv19@gmail.com>
2020-05-17 19:58:51 +0530

    for glitch

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const Datastore = require('nedb');
 
 const app = express();
-app.listen(3000, () => console.log('listening at 3000'));
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Starting server at ${port}`);
+});
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
 
