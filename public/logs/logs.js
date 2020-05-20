42bd96cae2c0862c3e4e40fbe487358865868fb9
shubham yadav <shubhamdv19@gmail.com>
2020-05-20 22:37:19 +0530

    final

--- public/logs/logs.js
+++ public/logs/logs.js
@@ -5,16 +5,24 @@ getData();
 
             for(item of data){
                 const root = document.createElement('div');
-                const mood = document.createElement('div');
-                mood.textContent = `mood: ${item.mood}`;
-                const geo = document.createElement('div')
-                geo.textContent = `${item.lat}, ${item.lon}`;
-                const date = document.createElement('div');
-                const dateString = new Date(item.timestamp).toLocaleString();
-                date.textContent =dateString;
+                
+                
                 const image = document.createElement('img');
+                
                 image.src = item.image64;
                 image.alt = 'Your selfies';
+                image.style.width
+                const mood = document.createElement('h5');
+                
+                mood.textContent = `mood: ${item.mood}`;
+                const geo = document.createElement('p');
+                
+                geo.textContent = `${item.lat}, ${item.lon}`;
+                const date = document.createElement('p');
+                
+                const dateString = new Date(item.timestamp).toLocaleString();
+                date.textContent =dateString;
+                
                 root.append(mood,geo,date,image);
                 document.body.append(root);
 
